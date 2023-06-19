a19404cb294562cdd1e7af18e5f74fdde71102eb
JaviVele <javivelez42@gmail.com>
2023-06-19 12:24:20 +0200

    primer commint

---
+++ config/http.js
@@ -0,0 +1,55 @@
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+module.exports.http = {
+  middleware: {
+    order: [
+      // ...
+      'bodyParser',
+      'cors',
+      // ...
+      'router',
+
+      
+      // ...
+    ],
+    
+    //bodyParser: require('body-parser').json(),
+    // ...
+    cors: (function() {
+      const corsOptions = {
+        origin: 'https://blabbersocial.netlify.app',
+        credentials: false,
+        methods: 'GET, POST, PUT, DELETE, OPTIONS, HEAD',
+        headers: 'Content-Type',
+        optionsSuccessStatus: 200,
+      };
+      return cors(corsOptions);
+    })(),
+    // bodyParser: (function _configureBodyParser(){
+    //   var skipper = require('skipper');
+    //   var middlewareFn = skipper({
+    //     strict: true,
+    //     // ... more Skipper options here ...
+    //   });
+    //   return middlewareFn;
+    // })(),
+    
+   
+  },
+
+  trustProxy: true
+  
+  // ...
+};
+
+
+module.exports.bodyParser ={
+  json: {
+    limit: '1mb',
+  },
+  urlencoded: {
+    limit: '1mb',
+    extended: true,
+  },
+}
